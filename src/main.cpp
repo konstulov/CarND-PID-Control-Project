e796176c121e137f8990d530cd427b1a7f5be1d2
konstulov <konstulov@gmail.com>
2020-12-07 18:14:15 -0800

    Refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,28 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
-/*
-const double tau_p = 0.2;
-const double tau_d = 3.0;
-const double tau_i = 0.004;
-*/
+// initial parameter values
+// these values were reached after 108 cycles of training starting at {0.1, 3.0, 0.004}
+const vector<double> init_p = {0.12, 3.4, 0.0045}; // {0.111518, 4.08299, 0.00403688}; // {0.123803, 3.3709, 0.00450544}
+// change in parameter values (for the Twiddle Algorithm)
+// converged to these values after 279 cycles of training starting at {0.01, 0.3, 0.0004};
+const vector<double> init_dp = {2.80662e-07, 1.0291e-05, 1.12265e-08}; // {0.03, 0.8, 0.001};
 // number of steps for one training cycle
-const int n_steps = 4000;
+const int n_steps = 2000;
 // number of steps for showing intermediate output (for progress tracking)
-const int n_show = 2000;
+const int n_show = 1000;
 // number of steps to look back for computing diff_cte (consecutive steps
 // might have too small of a difference to properly register change)
-const int n_diff = 5;
+const int n_diff = 4;
+// scale parameter for the inverse relationship b/w steering angle and throttle
+const double inv_scale = 10.0;
+// min/max throttle
+const double min_throttle = 0.3;
+const double max_throttle = 1.0;
+// max steering angle
+const double max_steer = 0.6;
+// Twiddle convergence criterion
+const double twiddle_eps = 0.00001;
  
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -45,6 +55,7 @@ string hasData(string s) {
   return "";
 }
 
+// add values of a vector
 double sum(const vector<double>& v) {
   double s = 0.0;
   for (int i=0; i<v.size(); i++) {
@@ -61,17 +72,21 @@ int main() {
    * TODO: Initialize the pid variable.
    */
   // parameter vector
-  // these values were reached after 108 cycles of training starting at {0.1, 3.0, 0.004}
-  vector<double> p = {0.123803, 3.3709, 0.00450544};
+  vector<double> p = init_p;
   // change in parameter vector (for the Twiddle Algorithm)
-  // converged to these values after 279 cycles of training starting at {0.01, 0.3, 0.0004};
-  vector<double> dp = {2.80662e-07, 1.0291e-05, 1.12265e-08};
+  vector<double> dp = init_dp;
   // CTE history vector - stores n_diff consecutive CTE values using circular indexing
   vector<double> ctes(n_diff, 0.0);
+
+  // cumulative CTE (cross-track error)
   double int_cte = 0.0;
+  // step counter
   int counter = 0;
+  // total error (sum of squares of CTE)
   double err = 0.0;
+  // best normalized error over one training cycle
   double best_err = std::numeric_limits<double>::infinity();
+  // boolean flag to toggle Twiddle algo b/w increasing and decreasing parameter values
   bool increase = true;
 
   h.onMessage([&pid, &p, &dp, &ctes, &int_cte, &counter, &err, &best_err, &increase](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
@@ -94,79 +109,86 @@ int main() {
           double angle = std::stod(j[1]["steering_angle"].get<string>());
           // index for storing the new CTE in the CTE history vector
           int idx = counter % n_diff;
+          // step within the current training cycle
+          int cycle_step = counter % n_steps;
+          
           if (counter == 0) {
             // initialize CTE history vector with the first CTE value
             ctes = vector<double>(n_diff, cte);
-          }
-          ctes[idx] = cte;
-          int cycle_step = counter % n_steps;
-          if (sum(dp) > 0.00001) {
-            // index of the currently optimized parameter
-            int cycle = counter / n_steps;
-            int i = cycle % dp.size();
-            if (cycle_step == 0) {
-              if (i == 0) {
-                std::cout << cycle << ": best_err = " << best_err << ", p = {" << p[0] << ", " << p[1] << ", " << p[2]
-                << "}, dp = {" << dp[0] << ", " << dp[1] << ", " << dp[2] << "}" << std::endl;
-              }
-              err = 0.0;
-              //prev_cte = cte;
-              int_cte = 0.0;
-              // new training cycle
-              if (increase) {
-                // start "twiddle up" cycle
-                p[i] += dp[i];
-              } else {
-                // start "twiddle down" cycle
-                p[i] -= 2*dp[i];
-              }
-              std::cout << counter << ": p[" << i << "] = " << p[i] << std::endl;
-            } else if (cycle_step == n_steps - 1) {
-              // completed n_steps of training
-              
-              // calculate average error over n_steps
-              double n_err = err / n_steps;
-              if (increase) {
-                // end of "twiddle up" cycle
-                if (n_err < best_err) {
-                  // achieved better error by increasing the current parameter
-                  best_err = n_err;
-                  dp[i] *= 1.1;
-                } else {
-                  // set incease to false since incrementing the parameter didn't lead to better error
-                  increase = false;
-                  // set back counter since we aren't done tuning the current parameter
-                  counter -= n_steps;
+          } else {
+            ctes[idx] = cte;
+            // Perform Twiddle Algorithm updates (until convergence)
+            if (sum(dp) > twiddle_eps) {
+              // current training cycle
+              int cycle = counter / n_steps;
+              // index of the currently optimized parameter
+              int i = cycle % dp.size();
+
+              if (cycle_step == 0) {
+                // completed n_steps of training -> perform Twiddle updates
+                
+                // calculate average error over n_steps
+                double n_err = err / n_steps;
+                if (i == 0) {
+                  // print out current parameter values at the end of each full training cycle
+                  std::cout << cycle << ": best_err = " << best_err << ", n_err = " << n_err
+                  << ", p = {" << p[0] << ", " << p[1] << ", " << p[2]
+                  << "}, dp = {" << dp[0] << ", " << dp[1] << ", " << dp[2] << "}" << std::endl;
                 }
-              } else {
-                // end of "twiddle down" cycle
-                if (n_err < best_err) {
-                  // achieved better error by decreasing the current parameter
-                  best_err = n_err;
-                  dp[i] *= 1.1;
+                if (increase) {
+                  // end of "twiddle up" cycle
+                  if (n_err < best_err) {
+                    // achieved better error by increasing the current parameter
+                    best_err = n_err;
+                    dp[i] *= 1.1;
+                  } else {
+                    // set incease to false since incrementing the parameter didn't lead to better error
+                    increase = false;
+                    // set back counter since we aren't done tuning the current parameter,
+                    // i.e. re-start the current training cycle with increase set to false
+                    counter -= n_steps;
+                  }
                 } else {
-                  // neither increasing nor decreasing the current parameter lead to better performance,
-                  // reset the parameter (to its previous value) and decrease the twiddle magnitude.
+                  // end of "twiddle down" cycle
+                  if (n_err < best_err) {
+                    // achieved better error by decreasing the current parameter
+                    best_err = n_err;
+                    dp[i] *= 1.1;
+                  } else {
+                    // neither increasing nor decreasing the current parameter lead to better performance,
+                    // reset the parameter (to its previous value) and decrease the twiddle magnitude.
+                    p[i] += dp[i];
+                    dp[i] *= 0.9;
+                  }
+                  // set incease to true for the next training cycle
+                  increase = true;
+                }
+                
+                // reset total squared error and cumulative error
+                err = 0.0;
+                int_cte = 0.0;
+                if (increase) {
+                  // start "twiddle up" cycle
                   p[i] += dp[i];
-                  dp[i] *= 0.9;
+                } else {
+                  // start "twiddle down" cycle
+                  p[i] -= 2*dp[i];
                 }
-                // set incease to true for the next training cycle
-                increase = true;
+                std::cout << counter << ": n_err = " << n_err << ", p[" << i << "] = " << p[i]
+                << ", dp[" << i << "] = " << dp[i] << std::endl;
               }
-              std::cout << counter << ": n_err = " << n_err << ", p[" << i << "] = " << p[i] << ", dp[" << i << "] = " << dp[i] << std::endl;
             }
           }
+          
+
           counter++;
           err += cte * cte;
           double prev_cte = ctes[(idx+1) % n_diff];
           double diff_cte = cte - prev_cte;
           int_cte += cte;
           double steer_value = -p[0] * cte - p[1] * diff_cte - p[2] * int_cte;
-          if (steer_value < -1.0) {
-            steer_value = -1.0;
-          } else if (steer_value > 1.0) {
-            steer_value = 1.0;
-          }
+          // clip steer value to be b/w -max_steer and max_steer
+          steer_value = std::min(max_steer, std::max(-max_steer, steer_value));
           if (counter % n_show == 0) {
             double cycle_err = err / (cycle_step+1);
             std::cout << "counter = " << counter << ", best_err = " << best_err << ", cycle_err = " << cycle_err << ", diff_cte = " << diff_cte << ", int_cte = " << int_cte << std::endl;
@@ -178,10 +200,13 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          // set throttle to be between min_throttle and max_throttle
+          //double throttle = min_throttle + (max_throttle - min_throttle) * (max_steer - fabs(steer_value)) / max_steer;
+          double throttle = min_throttle + (max_throttle - min_throttle) / (inv_scale * fabs(steer_value) + 1);
           
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = throttle; // 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
